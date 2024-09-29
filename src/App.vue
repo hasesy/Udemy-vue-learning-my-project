687cf2113935305890e3d5558b2de6c2535d01ed
Seoyeon Kim <kseoy0607@gmail.com>
2024-09-29 23:53:44 +0900

    240929 composition API watch, props

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch } from 'vue';
+import ChildComp from './components/ChildComp.vue';
 
 let count = ref(0);
 let title = 'Hello Vue';
+let color = ref('green');
 
 onMounted(() => {
   console.log('mounted');
@@ -11,12 +13,18 @@ onMounted(() => {
 const increseCount = () => {
   count.value++;
 };
+
+watch(color, () => {
+  console.log('color 변경');
+});
 </script>
 
 <template>
   <h1>{{ title }}</h1>
   <button @click="increseCount">count++</button>
   <p>{{ count }}</p>
+  <ChildComp :color="color" bgColor="yellow" />
+  <button @click="color = 'red'">change color</button>
 </template>
 
 <style scoped></style>
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p :style="{ color: color, backgroundColor: bgColor }">{{ color }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  color: String,
+  bgColor: String,
+});
+
+console.log(props.color);
+</script>
+
+<style></style>
